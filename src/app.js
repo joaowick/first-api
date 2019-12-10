73afdadadd828b88702cf82e0143f24aa3aaa83d
Joao Torres <joaotorrez14@gmail.com>
2019-12-10 10:58:55 -0300

    conexao database mongobd

--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+// Conectado
+db.on('connected', () => {
+  console.log('Mongoose está aberto!');
+});
+
+// Error
+db.on('error', err => {
+  console.log(`Mongoose deu pau!\n${err}`);
+});
+
+// Desconectado
+db.on('disconnect', () => {
+  console.log('Mongoose está fechado!');
+});
+
+process.on('SIGINT', () => {
+  db.close(() => {
+    console.log('Mongoose fechou de verdade!');
+    process.exit(0);
+  });
+});
 
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);
